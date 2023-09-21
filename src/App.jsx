da7f5ab540d6cf8ea11a7270b97ec007fceb4015
archanajyothy <archanajyothy@gmail.com>
2023-09-21 18:23:06 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const Container = styled.div`
   overflow-y: auto;
   scrollbar-width: none;
   color: white;
-  background: url()
+  background: url("./img/bg.jpeg");
   &::webkit-scrollbar{
     display: none;
   }
