732fbd03c08524eb86149244a23c0e85f034d310
archanajyothy <archanajyothy@gmail.com>
2023-09-22 00:39:15 +0530

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import Who from "./components/Who"
 import Works from "./components/Works"
 
 const Container = styled.div`
-  height : 100vh;
+  height: 100vh;
   scroll-snap-type: y mandatory;
-  scroll-behaviour: smooth;
+  scroll-behavior: smooth;
   overflow-y: auto;
   scrollbar-width: none;
   color: white;
   background: url("./img/bg.jpeg");
-  &::webkit-scrollbar{
+  &::-webkit-scrollbar{
     display: none;
   }
-`
+`;
 
 function App() {
 
